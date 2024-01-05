7b37d40d92102e6ddad4220404eb9c3431a455ef
JohnGastone <johngastone11@gmail.com>
2024-01-05 21:45:09 +0300

    launcher screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -5,6 +7,52 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: ListView(
+        children: [
+          SingleChildScrollView(
+            child: Container(
+              // height: 700,
+              padding: const EdgeInsets.only(top: 15),
+              decoration: const BoxDecoration(
+                  color: Color(0xFFEDECF2),
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(35),
+                      topRight: Radius.circular(35))),
+              child: Column(
+                children: [
+                  Container(
+                      margin:
+                          EdgeInsets.symmetric(vertical: 20, horizontal: 15),
+                      padding: EdgeInsets.all(10),
+                      child: Row(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                                color: Color(0xFF4C53A5),
+                                borderRadius: BorderRadius.circular(20)),
+                            child: Icon(
+                              Icons.add,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 10),
+                            child: Text(
+                              'Add A Gift Card',
+                              style: TextStyle(
+                                  color: Color(0xFF4C53A5),
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          )
+                        ],
+                      ))
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
