37c9db994c1a1a1cd5199e897c9fc42c8b1e827e
JohnGastone <johngastone11@gmail.com>
2024-01-23 08:33:12 +0300

    ++++++

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: sized_box_for_whitespace
+
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class Chart extends StatefulWidget {
   const Chart({super.key});
@@ -11,7 +14,11 @@ class _ChartState extends State<Chart> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Container(
+        width: double.infinity,
+        height: 300,
+        child: SFCartesianChart(),
+      ),
     );
   }
 }
